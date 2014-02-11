0de164473be06d7b763046a556fa3f68963c20be
cdelzenne <cd798@hotmail.com>
2014-02-11 17:23:36 +0800

    first draft pseudo code Rock Paper Scissors

---
+++ lesson6/labs/paper_scissors_stone/script.js
@@ -0,0 +1,18 @@
+Pseudo code Rock Paper Scissors
+
+1. Ask user to choose between "Rock" (enter #1) "Paper" (enter #2) "Scissors" (enter #3)
+2. Store choice "user-number" = 1 or 2 or 3  
+3. Randomly pick number between 0 and 1, Multiply by 4 and take absolute number
+5. Store number in "robot-number" = 1 or 2 or 3
+6. Display value of "robot-number"
+
+
+
+
+To go further:
+
+6. Compare "user-number" and "robot-number":
+	if "user-number" = "robot-number" display "draw"
+	if "user-number" = 1 and "robot-number" = 2 display "you win"
+	if "user-number" = 3 and "robot-number" = 2 display "you win"
+		...
